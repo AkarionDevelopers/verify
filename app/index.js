e065bed0ef244142a7dbae7ea1051b6843eb44ba
martinpam <pammesbergerma@gmail.com>
2021-11-11 11:43:05 +0100

    add new design for current logic

--- app/index.js
+++ app/index.js
@@ -6,6 +6,17 @@ import {
   getInvalidProps,
 } from './verify.js';
 
+const files = [];
+const $buttonBrowse = document.getElementById('buttonBrowse');
+const $buttonLearnMore = document.getElementById('buttonLearnMore');
+const $buttonNewVerification = document.getElementById('buttonNewVerification');
+const $uploadBox = document.getElementById('uploadBox');
+const $fileList = document.getElementById('fileList');
+const $instructionText = document.getElementById('instructionText');
+const $buttonReturnVerifier = document.getElementById('buttonReturnVerifier');
+const $arrowLeft = document.getElementById('arrowLeft');
+
+
 function parseAttachment(file) {
   return window.pdfjsLib
     .getDocument(file)
@@ -35,24 +46,32 @@ function verify(data) {
   return true;
 }
 
-function checkFile(file) {
+async function checkFile(file) {
   const $resultSuccess = document.getElementById('result-success');
   const $resultFail = document.getElementById('result-fail');
   const $resultText = document.getElementById('result-text');
-  return file.arrayBuffer()
+  let promise = new Promise((resolve,reject) =>{
+    file.arrayBuffer()
     .then(parseAttachment)
     .then(verify)
     .then(() => {
-      $resultSuccess.style.display = 'initial';
-      $resultFail.style.display = 'none';
-      $resultText.innerText = 'Verification successful. Check console for contents of verified object';
+    //  $resultSuccess.style.display = 'initial';
+     // $resultFail.style.display = 'none';
+    //  $resultText.innerText = 'Verification successful. Check console for contents of verified object';
+    console.log("verified now")
+    resolve("verified");
     })
     .catch((error) => {
       console.log('Verification failed', error);
-      $resultSuccess.style.display = 'none';
-      $resultFail.style.display = 'initial';
-      $resultText.innerText = 'Verification failed. Check console for details';
-    });
+    //  $resultSuccess.style.display = 'none';
+    //  $resultFail.style.display = 'initial';
+    //  $resultText.innerText = 'Verification failed. Check console for details';
+    reject("rejected");
+    });  
+  });
+  return promise;
+  
+  
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -66,10 +85,116 @@ document.addEventListener('DOMContentLoaded', () => {
   document.body.addEventListener('drop', (evt) => {
     evt.stopPropagation();
     evt.preventDefault();
-    checkFile(evt.dataTransfer.files[0]);
+    for (let i = 0; i < evt.dataTransfer.files.length;i++) {
+      files.push(evt.dataTransfer.files[i])
+    }
+    updateView();
   });
 
   document.getElementById('upload').addEventListener('change', (evt) => {
-    checkFile(evt.target.files[0]);
+    for (var file in evt.target.files) {
+      files[0].push(file)
+    }
   }, false);
 });
+
+$buttonBrowse.addEventListener('click', (evt) => {
+  console.log("clicked Browse File");
+});
+
+$buttonLearnMore.addEventListener('click', (evt) => {
+  console.log("clicked Learn More");
+});
+
+$buttonNewVerification.addEventListener('click', (evt) => {
+  files.length = 0;
+  updateView();
+});
+
+$buttonReturnVerifier.addEventListener('click', (evt) => {
+  files.length = 0;
+  updateView();
+});
+
+$arrowLeft.addEventListener('click', (evt) => {
+  files.length = 0;
+  updateView();
+});
+
+
+function updateView() {
+  if (files.length > 0) {
+    console.log("asdf")
+    $buttonBrowse.style.display = 'none';
+    $buttonLearnMore.style.display = 'none';
+    $uploadBox.style.display = 'none';
+    $buttonReturnVerifier.style.visibility = 'visible';
+    $arrowLeft.style.visibility = 'visible';
+    $buttonNewVerification.style.display = 'block';
+    $fileList.style.display = 'block';
+
+    updateFileList();
+
+  } else {
+    $buttonBrowse.style.display = 'block';
+    $buttonLearnMore.style.display = 'block';
+    $uploadBox.style.display = 'block';
+    $buttonNewVerification.style.display = 'none';
+    $buttonReturnVerifier.style.visibility = 'hidden';
+    $arrowLeft.style.visibility = 'hidden';
+    $instructionText.textContent = "Select a source to import your files";
+    $fileList.style.display = 'none';
+
+    resetFileList();
+  }
+}
+
+function updateFileList() {
+  console.log(files)
+  for (let i = 0; i < files.length; i++) {
+    
+    let $html = "";
+    $html += "<div id=\"fileOutline\">";
+    let isVerified, isPdf, isValidFormat;
+    isPdf = files[i].name.substr(name.length-4) == ".pdf";
+    isValidFormat = files[i].objectData != undefined;
+    checkFile(files[i]).then((res) => {isVerified = true;}, (err) => {isVerified = false;}).then(() =>{
+    $html += (isVerified ? 
+        "<div id=\"fileSymbolOuterSuccess\"> <div id=\"fileSymbol\">&#10003;</div> </div>" :
+        "<div id=\"fileSymbolOuterFailure\"> <div id=\"fileSymbol\">X</div> </div>") +
+      "<div id=\"fileStatusOuter\"> <div id=\"fileStatus\">" + 
+      (isVerified ? 
+        "Succesfully verified!" : 
+        "Verification failed!") +
+      "</div><div id=\"fileName\">" + 
+      files[i].name +
+      "</div>   </div> <div id=\"fileRightSegment\">" + 
+      (isVerified ?
+        "<div id=\"viewButton\"> View</div>" : 
+        !isPdf ? 
+          "<div id=\"noView\">Invalid file format</div>" : 
+            !isValidFormat ? 
+              "<div id=\"noView\">Invalid file content</div>":
+        "<div id=\"viewButton\"> View</div>") +
+      "</div></div>"
+      $fileList.innerHTML += $html;
+      console.log($html)
+
+
+    //update status text
+    if (files.length > 1) $instructionText.textContent = "Verification status of the uploaded documents.";
+    else if (isVerified) $instructionText.textContent = "Verification of the uploaded file was successful. View the content of the verified document.";
+    else $instructionText.textContent = "The verification of the uploaded file failed. View the content of the document for more details.";
+    });
+    
+    
+    
+    
+    
+    
+  }
+}
+
+function resetFileList() {
+  $fileList.innerHTML = "";
+}
